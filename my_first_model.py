a1db6bea4ccfcf85df8f2cf3524949e94c3264e4
Andrew <atursi@gmail.com>
2026-01-13 13:27:49 -0500

    old models as part of the old learning curicculum

---
+++ my_first_model.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([10, 20, 30, 40, 50])
+
+model = LinearRegression()
+model.fit(X, y)
+
+prediction = model.predict([[10]])
+
+print(f"AI Prediction for input 10: {prediction[0]}")
